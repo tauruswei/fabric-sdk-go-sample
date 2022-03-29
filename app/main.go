78b5ac916d904cb06e8420e687c4c96348fb3ac3
fengxiaoxiao <13156050650@163.com>
2022-03-29 10:11:07 +0800

    fabric-sdk-go sample test with network 1.4.2

--- app/main.go
+++ app/main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"log"
 	"fabric-sdk-go-sample/cli"
+	"log"
 )
 
 const (
-	cfgPath = "../config/config.yaml"
+	cfgPath = "/Users/fengxiaoxiao/work/go-projects/fabric-sdk-go-sample/config/config.yaml"
 )
 
 var (
@@ -14,7 +14,7 @@ var (
 )
 
 func main() {
-	org1Client := cli.New(cfgPath, "Org1", "Admin", "User1")
+	org1Client := cli.New(cfgPath, "Org1", "Admin", "Admin")
 	defer org1Client.Close()
 	// Install, instantiate, invoke, query
 	Phase(org1Client)
@@ -28,7 +28,7 @@ func Phase(cli1 *cli.Client) {
 		log.Panicf("Intall chaincode error: %v", err)
 	}
 	log.Println("Chaincode has been installed on org1's peers")
-	
+
 	// InstantiateCC chaincode only need once for each channel
 	if _, err := cli1.InstantiateCC("v1", peer0Org1); err != nil {
 		log.Panicf("Instantiated chaincode error: %v", err)
